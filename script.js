bc78fa99b6d688b59e401beb4fd6463fc3f0891b
Azkka azeem <azkaazeem804@gmail.com>
2026-03-20 10:20:23 +0500

    add more functionality

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 var main = document.querySelector("#main");
 var cursor = document.querySelector("#cursor");
+var imageDiv = document.querySelector("#image");
 
 main.addEventListener ("mousemove" , (dets) => {
-    console.log("Event Performed");
+    // console.log("Event Performed");
     gsap.to (cursor , {
         x: dets.x,
         y: dets.y,
+        duration: 0.15,
+    })
+})
+
+imageDiv.addEventListener ("mouseenter" , () => {
+    gsap.to (cursor, {
+        scale: 2,
+    })
+})
+
+imageDiv.addEventListener ("mouseleave" , () => {
+    gsap.to (cursor, {
+        scale: 1,
     })
 })
